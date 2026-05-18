6aef2b9a9120dff42cda77d58d04586a44cfcad3
msh <apple59082@naver.com>
2026-05-18 20:28:39 +0900

    [Add]player,Gamemanager

---
+++ Assets/Scripts/Managers/Singleton.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject(typeof(T).Name);
+                    instance = obj.AddComponent<T>();
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
